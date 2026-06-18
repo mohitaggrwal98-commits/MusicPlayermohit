f5a5716e2e9c64f549ce17bf24163457b7fdc560
mohitaggrwal98-commits <mohitaggrwal98@gmail.com>
2026-06-19 00:48:03 +0530

    Implement Flask server with home route
    Added Flask web server to provide a home route.

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+from flask import Flask
+import threading
+import os
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run():
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
+
+threading.Thread(target=run).start()
 """
 Music Player, Telegram Voice Chat Bot
 Copyright (c) 2021-present Asm Safone <https://github.com/AsmSafone>
